be7c0db9fd6857f60fee258beacc58208b29cdd1
Maverick <mavberk@gmail.com>
2022-03-25 20:40:38 -0500

    added the files

---
+++ simple-junit-examples/src/q1/computation/Main.java
@@ -0,0 +1,12 @@
+package q1.computation;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Computation x = new Computation();
+		System.out.println(x.getDiscount(100, 1));
+		System.out.println(x.getDiscount(101, 2));
+		System.out.println(x.getDiscount(102, 3));
+	}
+
+}
